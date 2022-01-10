6eb3fc545fcbf1e5ab9824c388815b0b3ec48eff
Matt <56481807+AnxietyMedicine@users.noreply.github.com>
2022-01-10 14:55:05 -0500

    Complete Project 1 (Base)

---
+++ WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Matt X on 1/8/22.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

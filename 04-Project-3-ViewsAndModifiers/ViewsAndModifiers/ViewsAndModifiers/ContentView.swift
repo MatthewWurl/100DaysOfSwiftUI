ab2f6f63ac6af477f6e864c4d4264733f6409c13
Matt <56481807+AnxietyMedicine@users.noreply.github.com>
2022-01-16 21:18:05 -0500

    Complete Project 3

---
+++ 04-Project-3-ViewsAndModifiers/ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
@@ -0,0 +1,79 @@
+//
+//  ContentView.swift
+//  ViewsAndModifiers
+//
+//  Created by Matt X on 1/15/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        ZStack {
+            LinearGradient(gradient: Gradient(colors: [.cyan, .blue]),
+                           startPoint: .top,
+                           endPoint: .bottom)
+                .ignoresSafeArea()
+            
+            VStack(spacing: 20) {
+                // Challenge 3 - Prominent title
+                Text("This is a prominent title!")
+                    .prominentTitle()
+                    .multilineTextAlignment(.center)
+                    .padding()
+                    
+                
+                // View modifier is passed directly to .modifier()
+                Text("Some text")
+                    .modifier(CustomCapsule(color: .blue))
+                
+                // Custom .customCapsule() view modifier
+                Text("Some other text")
+                    .customCapsule(color: .green)
+            }
+            .frame(maxWidth: .infinity)
+            .padding(.vertical, 80)
+            .background(.regularMaterial)
+            .clipShape(RoundedRectangle(cornerRadius: 25))
+            .shadow(radius: 10)
+            .padding(.horizontal)
+        }
+    }
+}
+
+struct CustomCapsule: ViewModifier {
+    let color: Color
+    
+    func body(content: Content) -> some View {
+        content
+            .font(.title)
+            .foregroundColor(.white)
+            .padding()
+            .background(color)
+            .clipShape(Capsule())
+    }
+}
+
+struct ProminentTitle: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .font(.largeTitle)
+            .foregroundColor(.blue)
+    }
+}
+
+extension View {
+    func customCapsule(color: Color) -> some View {
+        modifier(CustomCapsule(color: color))
+    }
+    
+    func prominentTitle() -> some View {
+        modifier(ProminentTitle())
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ 04-Project-3-ViewsAndModifiers/ViewsAndModifiers/ViewsAndModifiers/ViewsAndModifiersApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewsAndModifiersApp.swift
+//  ViewsAndModifiers
+//
+//  Created by Matt X on 1/15/22.
+//
+
+import SwiftUI
+
+@main
+struct ViewsAndModifiersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

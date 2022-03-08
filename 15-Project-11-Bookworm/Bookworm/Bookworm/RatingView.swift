4ebd09ec8656a4d8dc98dc00c44882e5d582fa66
Matt <56481807+AnxietyMedicine@users.noreply.github.com>
2022-03-08 16:37:51 -0500

    Add accessibility to Bookworm (Project 15 challenge)

--- 15-Project-11-Bookworm/Bookworm/Bookworm/RatingView.swift
+++ 15-Project-11-Bookworm/Bookworm/Bookworm/RatingView.swift
@@ -33,6 +33,25 @@ struct RatingView: View {
                     }
             }
         }
+        .accessibilityElement()
+        .accessibilityLabel("Rating")
+        .accessibilityValue(
+            rating == 1 ? "1 star" : "\(rating) stars"
+        )
+        .accessibilityAdjustableAction { direction in
+            switch direction {
+            case .increment:
+                if rating < maximumRating {
+                    rating += 1
+                }
+            case .decrement:
+                if rating > 1 {
+                    rating -= 1
+                }
+            default:
+                break
+            }
+        }
     }
     
     func image(for number: Int) -> Image {

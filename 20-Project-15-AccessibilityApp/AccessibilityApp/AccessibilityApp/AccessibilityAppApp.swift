aaeccb2fecf61c051a18ef66065e55f1aab13f41
Matt <56481807+AnxietyMedicine@users.noreply.github.com>
2022-03-09 16:40:51 -0500

    Complete Project 15

---
+++ 20-Project-15-AccessibilityApp/AccessibilityApp/AccessibilityApp/AccessibilityAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AccessibilityAppApp.swift
+//  AccessibilityApp
+//
+//  Created by Matt X on 3/8/22.
+//
+
+import SwiftUI
+
+@main
+struct AccessibilityAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ 20-Project-15-AccessibilityApp/AccessibilityApp/AccessibilityApp/ContentView.swift
@@ -0,0 +1,44 @@
+//
+//  ContentView.swift
+//  AccessibilityApp
+//
+//  Created by Matt X on 3/8/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    let pictures = [
+        "ales-krivec-15949",
+        "galina-n-189483",
+        "kevin-horstmann-141705",
+        "nicolas-tissot-335096"
+    ]
+    
+    let labels = [
+        "Tulips",
+        "Frozen tree buds",
+        "Sunflowers",
+        "Fireworks"
+    ]
+    
+    @State private var selectedPicture = Int.random(in: 0...3)
+    
+    var body: some View {
+        Image(pictures[selectedPicture])
+            .resizable()
+            .scaledToFit()
+            .onTapGesture {
+                selectedPicture = Int.random(in: 0...3)
+            }
+            .accessibilityLabel(labels[selectedPicture])
+            .accessibilityAddTraits(.isButton)
+            .accessibilityRemoveTraits(.isImage)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
